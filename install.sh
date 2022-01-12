80f0799dae360c14e0ff5f561bb4f79ad208bd46
Dup4 <lyuzhi.pan@gmail.com>
2022-01-12 17:35:39 +0800

    feat: update llvm

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ TOP_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH"
 
+LLVM_VERSION=13
+
 # Install dependencies
 apt-get clean
 apt-get update
@@ -22,12 +24,15 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BA6932366A7557
 
 # Add sources
 echo "deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu focal main" >/etc/apt/sources.list.d/ubuntu-toolchain-r-ubuntu-test-focal.list
-echo "deb http://apt.llvm.org/focal/ llvm-toolchain-focal-11 main" >/etc/apt/sources.list.d/llvm.list
+
+echo "deb http://apt.llvm.org/focal/ llvm-toolchain-focal-${LLVM_VERSION} main" >/etc/apt/sources.list.d/llvm.list
+echo "deb-src http://apt.llvm.org/focal/ llvm-toolchain-focal-${LLVM_VERSION} main" >>/etc/apt/sources.list.d/llvm.list
+
 echo "deb http://ppa.launchpad.net/deadsnakes/ppa/ubuntu focal main" >/etc/apt/sources.list.d/python.list
 
 apt-get update
 
-deps="make g++-11 gcc-11 clang-11 libc++-11-dev libc++abi-11-dev python3.9"
+deps="make g++-11 gcc-11 clang-${LLVM_VERSION} libc++-${LLVM_VERSION}-dev libc++abi-${LLVM_VERSION}-dev lldb-$LLVM_VERSION lld-$LLVM_VERSION clangd-$LLVM_VERSION python3.9"
 
 for pkg in ${deps}; do
     install_ok='n'
@@ -49,7 +54,7 @@ done
 
 ln -s /usr/bin/g++-11 /usr/local/bin/g++
 ln -s /usr/bin/gcc-11 /usr/local/bin/gcc
-ln -s /usr/bin/clang-11 /usr/local/bin/clang
-ln -s /usr/bin/clang++-11 /usr/local/bin/clang++
+ln -s /usr/bin/clang-${LLVM_VERSION} /usr/local/bin/clang
+ln -s /usr/bin/clang++-${LLVM_VERSION} /usr/local/bin/clang++
 
 apt-get clean
