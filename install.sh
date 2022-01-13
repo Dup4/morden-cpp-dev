581cbf9b26d0c7d73a3caa13106a5017a7ecb896
Dup4 <lyuzhi.pan@gmail.com>
2022-01-13 10:24:16 +0800

    fix: remove python-clang

--- install.sh
+++ install.sh
@@ -50,7 +50,6 @@ libclang-common-${LLVM_VERSION}-dev \
 libclang-${LLVM_VERSION}-dev \
 libclang1-${LLVM_VERSION} \
 clang-format-${LLVM_VERSION} \
-python-clang-${LLVM_VERSION} \
 clangd-${LLVM_VERSION} \
 libfuzzer-${LLVM_VERSION}-dev \
 lldb-${LLVM_VERSION} \
