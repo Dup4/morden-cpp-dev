bf3504b887361cba7a25ebc29b9c7b05c7c66877
Dup4 <lyuzhi.pan@gmail.com>
2022-12-24 19:13:53 +0800

    fix: use -f for ln

--- scripts/install_cmake.sh
+++ scripts/install_cmake.sh
@@ -57,6 +57,6 @@ curl -sSL "https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/
 sha256sum -c --ignore-missing "${CMAKE_CHECKSUM_NAME}"
 sh "${TMP_DIR}/${CMAKE_BINARY_NAME}" --prefix=/opt/cmake --skip-license
 
-ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
+ln -sf /opt/cmake/bin/cmake /usr/local/bin/cmake
 
 rm -rf "${TMP_DIR}"
