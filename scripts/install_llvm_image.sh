686ff3fa208996d9bd1009c4903ef0081cd06e3a
Dup4 <lyuzhi.pan@gmail.com>
2022-12-25 23:54:02 +0800

    feat: llvm

---
+++ scripts/install_llvm_image.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+set -e
+
+# shellcheck disable=SC2034
+TOP_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
+bash "${TOP_DIR}"/install_common.sh
+bash "${TOP_DIR}"/install_cpp_common.sh
+bash "${TOP_DIR}"/install_llvm.sh
+bash "${TOP_DIR}"/install_cmake.sh
